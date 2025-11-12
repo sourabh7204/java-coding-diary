7a48ec18499c258bb14a37e1d270966d955baf6f
sourabh7204 <sourabh722004@gmail.com>
2025-11-12 21:02:26 +0530

    Used Data Types

---
+++ DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Bytes 
+        byte b=7;
+        System.out.println(b);
+        //Character a single value will be stored
+        char ch='s';
+        System.out.println(ch);
+        //Bollean Value only we can assign 0 that is false and 1 is true
+        boolean var =false;
+        System.out.println(var);
+
+        //Float is used for storing a decimal number
+        float price = 10.6f;
+        System.out.println(price);
+
+        //Integer Number 
+        int number = 108;
+        System.out.println(number);
+
+        //long - is used for large number like 87237297294242
+
+        //double - is used for storing large numer in decimals like 8923824242424.5
+
+        //Short - We won't use it frequltly 
+        short n=240; 
+        System.out.println(n);
+
+    }
+}

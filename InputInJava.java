dc2c84f2852ff54e249633d0593fd32da745dccd
sourabh7204 <sourabh722004@gmail.com>
2025-11-14 22:03:10 +0530

    Learned About Types on Input in the Java

--- InputInJava.java
+++ InputInJava.java
@@ -5,20 +5,20 @@ public class InputInJava {
         Scanner sc=new Scanner(System.in);
 
         //It will print the String upto a word or sapce only it won't print the line after it
-        //String input = sc.next();
-        //System.out.println(input);
+        String input = sc.next();
+        System.out.println(input);
 
         //If we use nextLine then the full line of input will be displayed   
-        //String name =sc.nextLine();
-        //System.out.println(name);
+        String name =sc.nextLine();
+        System.out.println(name);
 
         //It will print a number till space eg:7238 it won't display the number like 723 2942 2937
-        //  int number = sc.nextInt();
-        //  System.out.println(number);
+         int number = sc.nextInt();
+         System.out.println(number);
 
         //nextBoolean - will display only true or false 
-        // boolean decimal =sc.nextBoolean();
-        // System.out.println(decimal);
+        boolean decimal =sc.nextBoolean();
+        System.out.println(decimal);
 
         //nextShort - 
         short nxtsort=sc.nextShort();

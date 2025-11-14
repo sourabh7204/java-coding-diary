e1fde3f6f3fae6d6ffcf9f02b5f8e8812fddb00b
sourabh7204 <sourabh722004@gmail.com>
2025-11-14 22:55:48 +0530

    Calculated Area of Circle

--- sumofab.java
+++ sumofab.java
@@ -1,9 +1,30 @@
+import java.util.*;
+
 public class sumofab {
 
     public static void main(String[] args) {
-        int a=5;
-        int  b=10;
-        int sum=a+b;
-        System.out.println(sum);
+        // int a=5;
+        // int  b=10;
+        // int sum=a+b;
+        // System.out.println(sum);
+
+
+        //Sum of a & b using Input Opertaion
+        // Scanner sc= new Scanner(System.in);
+        //   System.out.println("Enter the value of a:");
+        // int a =sc.nextInt();
+        // System.out.println("Enter the value of b:");
+        // int b=sc.nextInt();
+        // int sum=a*b;
+        // System.out.println("The product of a and b is: " +sum);
+
+        //Code to find Area of Circle
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value of radius:");
+        int rad = sc.nextInt();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+
+
     }
 }

5f20607e5322e9d62d0e06b8b4104717d41c7adf
sourabh7204 <sourabh722004@gmail.com>
2025-11-17 23:26:54 +0530

    Logical Operaors wih AND, OR , NOT

---
+++ LogicalOperator.java
@@ -0,0 +1,15 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=4;
+
+        //Logical AND (&&)
+        System.out.println( (a>b) && (a<b));
+
+        //Logical OR (||)
+          System.out.println( (a>b) || (a<b));
+
+          //Logical NOT (!)
+            System.out.println( !(a<b));
+    }
+}

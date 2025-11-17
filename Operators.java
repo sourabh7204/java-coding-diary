2fbc3b23655c68623270c3c0022f4e3abce86fbb
sourabh7204 <sourabh722004@gmail.com>
2025-11-17 22:46:19 +0530

    Just tried Operators

---
+++ Operators.java
@@ -0,0 +1,13 @@
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int A=10;
+        int B=5;
+        System.out.println("Addition:"+(A+B));
+        System.out.println("Substraction:"+(A-B));
+        System.out.println("Muliplication:"+(A*B));
+        System.out.println("Division:"+(A/B));
+        System.out.println("Modulo (reminder):"+(A%B));
+    }
+}

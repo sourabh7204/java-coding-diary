ef5a97c44e94f97baebc7c2d2d7ffd61eed7a129
sourabh7204 <sourabh722004@gmail.com>
2025-11-17 22:59:00 +0530

    Worked with unary operators

--- Operators.java
+++ Operators.java
@@ -2,12 +2,37 @@
 public class Operators {
 
     public static void main(String[] args) {
-        int A=10;
-        int B=5;
-        System.out.println("Addition:"+(A+B));
-        System.out.println("Substraction:"+(A-B));
-        System.out.println("Muliplication:"+(A*B));
-        System.out.println("Division:"+(A/B));
-        System.out.println("Modulo (reminder):"+(A%B));
+
+        //Binary Operators
+        // int A=10;
+        // int B=5;
+        // System.out.println("Addition:"+(A+B));
+        // System.out.println("Substraction:"+(A-B));
+        // System.out.println("Muliplication:"+(A*B));
+        // System.out.println("Division:"+(A/B));
+        // System.out.println("Modulo (reminder):"+(A%B));
+
+        //Unary Operators
+        // int a=10;
+        // int b=++a; //Pre Increment
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        // int a=10;
+        // int b=a++; //Post Increment
+        // System.out.println(a);
+        // System.out.println(b);
+    
+        //  int a=10;
+        // int b=--a; //Pre Decrement
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        int a=10;
+        int b=a--; //Post Decrement
+        System.out.println(a);
+        System.out.println(b);
     }
 }

4cf705095dbb596fa4e635dcdb4302d76a94b517
sourabh7204 <sourabh722004@gmail.com>
2025-11-17 23:10:42 +0530

    Realtional Operator was cool

---
+++ RelationalOperator.java
@@ -0,0 +1,19 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int A=10;
+        int B=5;
+        
+        //Equal to 
+        System.out.println(A==B);
+        //Not Equal to 
+        System.out.println(A!=B);
+        //Greater Than
+        System.out.println(A>B);
+        //Lesser Than
+        System.out.println(A<B);
+        ////Greater than and equal to
+        System.out.println(A>=B); //if any one condition is true the op will be true orelse op will be false
+        //Lesser than and equal to
+        System.out.println(A<=B);
+    }
+}

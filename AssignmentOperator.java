a2a3da4c3ecc1be31e3cee065594a2a03659db64
sourabh7204 <sourabh722004@gmail.com>
2025-11-17 23:35:57 +0530

    Assignment Operator

---
+++ AssignmentOperator.java
@@ -0,0 +1,24 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a=10;
+
+        // a=a+10;
+
+        //Addition
+        a+=10;
+        System.out.println( a);
+
+        //Sub
+        a-=5;
+        System.out.println( a);
+
+
+        //Mul
+        a*=5;
+        System.out.println( a);
+
+        //Div
+        a/=5;
+        System.out.println( a);
+    }   
+}
